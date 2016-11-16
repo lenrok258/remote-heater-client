8118c3456283da1c6a7251221fabd44b2e40fedd
Kornel Zemla <kornelzemla@gmail.com>
2016-11-16 01:47:53 +0100

    Mock temp sensor

--- temp_sensor.py
+++ temp_sensor.py
@@ -1,8 +1,14 @@
 from logger import Logger
 
+logger = Logger(__name__)
+
+
 class TempSensor:
-    
+    def __init__(self):
+        pass
+
     @property
-    def value():
-        return 20.1
-        
+    def value(self):
+        temp_mock = 20.1;
+        logger.info("Temperature measured is {}".format(temp_mock))
+        return temp_mock
