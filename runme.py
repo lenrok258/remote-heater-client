7bb0f05bc478ad7c12f3b9e8b3b2145fb69b7efe
Kornel Zemla <kornelzemla@gmail.com>
2016-11-18 21:28:21 +0100

    Actions enum, start_looper function

--- runme.py
+++ runme.py
@@ -3,15 +3,21 @@
 import time
 
 import requests
+from enum import Enum
 
 from logger import ErrorId
 from logger import Logger
 from temp_sensor import TempSensor
 
-REQUEST_INTERVAL_SEC = 5
+REQUEST_INTERVAL_SEC = 60
 
 logger = Logger(__name__)
 temp_sensor = TempSensor()
+
+
+class Actions(Enum):
+    TURN_ON_HEATER = 'TURN_ON_HEATER'
+    TURN_OFF_HEATER = 'TURN_OFF_HEATER'
 
 
 def sleep(seconds):
@@ -24,11 +30,16 @@ def send_request(current_temp):
     return response.json()
 
 
-while True:
-    try:
-        current_temp = temp_sensor.value()
-        response = send_request(current_temp)
-    except Exception as e:
-        logger.error("Error while getting response from server={}".format(e), ErrorId.SERVER_CONNECTION_ERROR)
+def start_looper():
+    while True:
+        try:
+            current_temp = temp_sensor.value()
+            response = send_request(current_temp)
+        except Exception as e:
+            logger.error("Error while getting response from server={}".format(e), ErrorId.SERVER_CONNECTION_ERROR)
 
-    sleep(REQUEST_INTERVAL_SEC)
+        sleep(REQUEST_INTERVAL_SEC)
+
+
+if __name__ == '__main__':
+    start_looper()
