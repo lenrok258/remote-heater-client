7a071e2020f77c7c731446ab524d65e047575b12
Kornel Zemla <kornelzemla@gmail.com>
2016-11-22 20:53:34 +0100

    Heater pt 2

--- heater.py
+++ heater.py
@@ -1,9 +1,24 @@
-from logger import Logger
 import RPi.GPIO as GPIO
 
-logger = Logger(__name__)
+from config.config import config
+from logger import Logger
 
 
 class Heater:
+    logger = Logger(__name__)
+    GPIO_1 = config['heater']['gpio-1']
+    GPIO_2 = config['heater']['gpio-2']
+    GPIOS = [GPIO_1, GPIO_2]
+
     def __init__(self):
         pass
+
+    def turn_on(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Heater.GPIOS, GPIO.OUT)
+        GPIO.output(Heater.GPIOS, GPIO.HIGH)
+
+    def turn_off(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Heater.GPIOS, GPIO.OUT)
+        GPIO.output(Heater.GPIOS, GPIO.LOW)
