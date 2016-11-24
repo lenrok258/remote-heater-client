f68264314f97452dad8540c7708abfe434adb5e2
kornel.zemla <kornel.zemla@ocado.com>
2016-11-24 11:43:20 +0100

    logging fixed, exec from cron fixed

--- pull-restart.sh
+++ pull-restart.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+LOG_FILE=./logs/`date +%Y-%m-%d_%H:%M.log`
+
+function log {
+    echo $1 | tee -a $LOG_FILE
+}
+
 ANY_CHANGES=$(git fetch --dry-run 2>&1)
 
 if [ ! -z "${ANY_CHANGES}" ]; then
-    echo 'New changes found on git. About to pull them and restart Remote Heater client';
-    killall runme.py
-    git pull --force
+    log 'New changes found on git. About to pull them and restart Remote Heater client';
+    killall runme.py | tee -a $LOG_FILE
+    git pull --force | tee -a $LOG_FILE
 fi;
 
 RUNME_RUNNING=$(ps cax | grep runme.py)
 
 if [ -z "${RUNME_RUNNING}" ]; then
-    echo 'Remote Heater client does not seem to work. About to start it.';
-    ./install.sh
-    ./runme.py
+    log 'Remote Heater client does not seem to work. About to start it.';
+    ./install.sh | tee -a $LOG_FILE
+    ./.env/bin/python -u ./runme.py | tee -a $LOG_FILE
 fi;
--- run-cron.sh
+++ run-cron.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-LOG_FILE=./logs/`date +%Y-%m-%d_%H:%M.log`
-
 cd $(dirname $0)
 
 source $HOME/.profile
 
-./pull_restart.sh | tee $LOG_FILE 2>&1
+./pull-restart.sh
