3d0842c79597f04a468e6152ba16904fc4fdd35d
Kornel Zemla <kornelzemla@gmail.com>
2016-11-25 14:15:41 +0100

    Fixing problem with run-cron.sh

--- pull-restart.sh
+++ pull-restart.sh
@@ -10,7 +10,7 @@ ANY_CHANGES=$(git fetch --dry-run 2>&1)
 
 if [ ! -z "${ANY_CHANGES}" ]; then
     log 'New changes found in git. About to pull them and restart Remote Heater client';
-    kill $(ps aux | grep -v grep | grep "./run-remote-heater-client.sh" | awk '{print $2}') | tee -a ${LOG_FILE}
+    kill $(ps aux | grep -v "grep" | grep "remote-heater-client" | awk '{print $2}') | tee -a ${LOG_FILE}
     sleep 5
     git reset --hard | tee -a ${LOG_FILE}
     git pull --force | tee -a ${LOG_FILE}
@@ -18,11 +18,11 @@ else
     log 'No new changes found in git'
 fi;
 
-IS_RUNNING=$(ps aux | grep -v grep | grep "./run-remote-heater-client.sh")
+IS_RUNNING=$(ps aux | grep -v grep | grep "remote-heater-client")
 
 if [ -z "${IS_RUNNING}" ]; then
     log 'Remote Heater client does not seem to work. About to start it.';
-    ./install.sh | tee -a ${LOG_FILE}p
+    ./install.sh | tee -a ${LOG_FILE}
     ./run-remote-heater-client.sh
 else
     log 'Remote Heater already started - nothing to do.'
--- run-remote-heater-client.sh
+++ run-remote-heater-client.sh
@@ -2,5 +2,5 @@
 
 LOG_FILE=./logs/run_`date +%Y-%m-%d_%H:%M.log`
 
-./.env/bin/python -u ./runme.py | tee -a ${LOG_FILE}
+./.env/bin/python -u ./remote-heater-client.py | tee -a ${LOG_FILE}
 
